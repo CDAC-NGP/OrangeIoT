2907f72b72f8ea75bfe2555db469052f95ed55db
AbhishekSoftPoly <abhishek.angole@gmail.com>
2023-11-25 21:52:28 +0530

    local to git

--- Codes/OrangeServer.py
+++ Codes/OrangeServer.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 iotData=[ 
      {'id':'o101',
-     'Sensor': {'RFID':'11002487487','AnalogPin 1': '1024','AnalogPin 2': '1024','DigitalIn 1': 'High','DigitalIn 2': 'Low'},
-     'Actuator': {'Servo 1':'180','Servo 2':'180','Digital Out 1':'High','Digital Out 2':'High'}
+     'Sensor': {'RFID':'11002480000','AnalogPin 1': '1024','AnalogPin 2': '1024','DigitalIn 1': 'High','DigitalIn 2': 'Low'},
+     'Actuator': {'Servo 1':'180','Servo 2':'180','Digital Out 1':'0','Digital Out 2':'0'}
      },
      
      {'id':'o102',
@@ -16,7 +16,7 @@ iotData=[
      },
      
      {'id':'o103',
-     'Sensor': {'RFID':'11002487487','AnalogPin 1': '1024','AnalogPin 2': '1024','DigitalIn 1': 'High','DigitalIn 2': 'Low'},
+     'Sensor': {'RFID':'11002487488','AnalogPin 1': '1024','AnalogPin 2': '1024','DigitalIn 1': 'High','DigitalIn 2': 'Low'},
      'Actuator': {'Servo 1':'180','Servo 2':'180','Digital Out 1':'High','Digital Out 2':'High'}
      } 
 ]
@@ -32,7 +32,7 @@ def senData():
     Analog2 = request.args['a2']
     Digital1 = request.args['di1']
     Digital2 = request.args['di2']
-    print(nodeId,RFID,Analog1,Analog2,Digital1,Digital2)
+    # print(nodeId,RFID,Analog1,Analog2,Digital1,Digital2)
     
     ###/******Update the Sensor Values ****/###    
     for i in range(len(iotData)):
@@ -42,14 +42,14 @@ def senData():
             iotData[i]['Sensor']['AnalogPin 2']  = Analog2
             iotData[i]['Sensor']['DigitalIn 1']  = Digital1
             iotData[i]['Sensor']['DigitalIn 2']  = Digital2
+            
+            response = jsonify({'allData':iotData[i]['Actuator']})
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            
+            print(i,nodeId,RFID)
             break
 
-    ###/****************************** ****/###
-    
-    # print(iotData)
-          
-    response = jsonify({'allData':iotData[0]['Actuator']})
-    response.headers.add('Access-Control-Allow-Origin', '*')
+   
     return response
 
 @app.route('/actData',methods=['GET'])
@@ -59,7 +59,7 @@ def actData():
     Servo2 = request.args['s2']
     dOut1 = request.args['do1']
     dOut2 = request.args['do2']
-    print(nodeId,Servo1,Servo2,dOut1,dOut2)
+    # print(nodeId,Servo1,Servo2,dOut1,dOut2)
     
     for i in range(len(iotData)):
         if iotData[i]['id']==nodeId:
@@ -69,8 +69,8 @@ def actData():
             iotData[i]['Actuator']['Digital Out 2']  = dOut2
     
     
-    response = jsonify({'allData':iotData[0]['Sensor']})
-    response.headers.add('Access-Control-Allow-Origin', '*')
+            response = jsonify({'allData':iotData[i]['Sensor']})
+            response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 
